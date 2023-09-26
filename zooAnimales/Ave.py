7c4e0fd1ac7ced020a1c94cedf2212aedaebc83d
manuela-Cha <141794878+manuela-Cha@users.noreply.github.com>
2023-09-26 10:29:13 -0500

    Update Ave.py

--- zooAnimales/Ave.py
+++ zooAnimales/Ave.py
@@ -5,7 +5,7 @@ class Ave(Animal):
     aguilas = 0
     total_aves = 0
 
-    def __init__(self, nombre = "", edad = 0, habitat = "", genero = "", colorPlumas):
+    def __init__(self, nombre = "", edad = 0, habitat = "", genero = "", colorPlumas = ""):
         super().__init__(nombre, edad, habitat, genero)
         self._colorPlumas = colorPlumas
         Ave._listado.append(self)
