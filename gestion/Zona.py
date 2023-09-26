7f9a4dda2efa2bb2a858d69118047a485d24dc29
manuela-Cha <141794878+manuela-Cha@users.noreply.github.com>
2023-09-26 11:32:48 -0500

    Update Zona.py

--- gestion/Zona.py
+++ gestion/Zona.py
@@ -1,15 +1,29 @@
 class Zona:
-    nombre = ""
-    zoo = []
-    animales = []
-    from zooAnimales import Animal
-
-    def __init__(self, nombre, zoo):
+    def __init__(self, nombre = "", zoo = [], animales = []):
         self._nombre = nombre
         self._zoo = zoo
+        self._animales = animales
 
-    def agregarAnimales():
-        print ("lol")
+    def agregarAnimales(self, animal):
+        self._animales.append(animal)
     
     def cantidadAnimales():
-       print ("lol")
+        return len(self._animales)
+
+    def setNombre(self, nombre):
+        self._nombre = nombre
+
+    def getNombre (self):
+        return self._nombre
+
+    def setZoo(self, zoo):
+        self._zoo = zoo
+
+    def getZoo(self):
+        return self._zoo
+
+    
+
+
+            
+            
