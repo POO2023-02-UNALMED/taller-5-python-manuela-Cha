67e12965fc726b6dd61b76f6c83689f8f81ef109
manuela-Cha <141794878+manuela-Cha@users.noreply.github.com>
2023-09-26 06:57:57 -0500

    Update Zoologico.py

--- Zoologico.py
+++ Zoologico.py
@@ -1,1 +1,16 @@
+class Zoologico:
+    nombre = ""
+    ubicacion = ""
+    zonas = []
+    totalAnimales = 0
 
+    def __init__(self, nombre, ubicacion):
+        self._nombre = nombre
+        self._ubicacion = ubicacion
+
+
+    def agregarZonas(zona):
+        Zoologico.zonas.append(zona)
+
+    def cantidadTotalAnimales(self):
+        for zona in zonas:
