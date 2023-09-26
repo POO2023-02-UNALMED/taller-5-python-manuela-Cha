07ec0d8bad3e89408054c21ad14ed9ef651d33bd
manuela-Cha <141794878+manuela-Cha@users.noreply.github.com>
2023-09-26 06:56:42 -0500

    Create Zona.py

---
+++ Zona.py
@@ -0,0 +1,15 @@
+class Zona:
+    nombre = ""
+    zoo = []
+    animales = []
+    from zooAnimales import Animal
+
+    def __init__(self, nombre, zoo):
+        self._nombre = nombre
+        self._zoo = zoo
+
+    def agregarAnimales():
+        print ("lol")
+    
+    def cantidadAnimales():
+       print ("lol")
