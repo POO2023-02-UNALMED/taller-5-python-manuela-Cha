b2680a5f45b91e1fc75f2191fabcf97b10e73864
manuela-Cha <141794878+manuela-Cha@users.noreply.github.com>
2023-09-26 12:39:49 -0500

    Update Zoologico.py

--- gestion/Zoologico.py
+++ gestion/Zoologico.py
@@ -5,9 +5,25 @@ class Zoologico:
         self._ubicacion = ubicacion
         self._zonas = zonas 
 
+    def setNombre (self, nombre):
+        self._nombre = nombre 
+
+    def getNombre (self):
+        return self._nombre
+
+
+    def setUbicacion (self, ubi):
+        self._ubicacion = ubi
+
+    def getUbicacion (self):
+        return self._ubicacion
+
 
     def agregarZonas(zona):
         self._zonas.append(zona)
+
+    def getZona(self):
+        return self._zonas
 
     def cantidadTotalAnimales(self):
         total_animales = 0
