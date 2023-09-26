9d1464b3a8c23b86424eeeb18008e97bf54375cb
manuela-Cha <141794878+manuela-Cha@users.noreply.github.com>
2023-09-26 09:53:24 -0500

    Update Ave.py

--- zooAnimales/Ave.py
+++ zooAnimales/Ave.py
@@ -11,16 +11,16 @@ class Ave(Animal):
         Ave._listado.append(self)
         Ave.total_aves += 1
 
-    classmethod
+    @classmethod
     def cantidadAves (cls):
         return cls.total_aves
     
-    classmethod
+    @classmethod
     def crearHalcon(cls, nombre = "", edad = 0, habitat = "", genero = "" ):
         cls.halcones += 1
         halcon = Ave(nombre, edad, "montanas", genero, "cafe glorioso")
 
-    classmethod
+    @classmethod
     def crearAguila(cls, nombre = "", edad = 0, habitat = "", genero = "" ):
         cls.aguilas += 1
         aguila = Ave(nombre, edad, "montanas", genero, "blanco y amarillo")
