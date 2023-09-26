f84b2a99d6697d4b04d4c78758024aa2a439cad2
manuela-Cha <141794878+manuela-Cha@users.noreply.github.com>
2023-09-26 10:10:12 -0500

    Update Reptil.py

--- zooAnimales/Reptil.py
+++ zooAnimales/Reptil.py
@@ -1,60 +1,44 @@
-class Animal:
-    totalAnimales = 0
+import Animal
+class Reptil(Animal):
+    _listado = []
+    iguanas = 0
+    serpientes = 0
+    total_reptiles = 0
 
-    def __init__(self, nombre =  "", edad = 0, habitat = "", genero = ""):
-        self._nombre = nombre
-        self._edad = edad
-        self._habitat = habitat
-        self._genero = genero
-        self._zona = []
-        totalAnimales += 1
-        Animal.zona.append(self)
-
-    def movimiento (self):
-        return "desplazamiento"
+    def __init__(self, colorEscamas, largoCola):
+        super().__init__(nombre = "", edad = 0, habitat = "", genero = "")
+        self._colorEscamas = colorEscamas
+        self._largoCola = largoCola 
+        Reptil._listado.append(self)
+        Reptil.total_reptiles += 1
 
     @classmethod
-    def totalPorTipo(cls):
-        from zooAnimales.mamifero import Mamifero
-        from zooAnimales.ave import Ave
-        from zooAnimales.reptil import Reptil
-        from zooAnimales.pez import Pez
-        from zooAnimales.anfibio import Anfibio
-
-        mam  = Mamifero.cantidadMamiferos()
-        av = Ave.cantidadAves()
-        pe = Pez.cantidadPeces()
-        an = Anfibio.cantidadAnfibios()
-
-        return f'''Mamiferos : {cantidadMamiferos}\nAves : {cantidadAves}\nReptiles : {cantidadReptiles}\nPeces : {cantidadPez}\nAnfibios : {cantidadAnfibios}'''
-
-    def __str__(self):
-        if len(self._zona) != 0:
-            nombre = self._zona.__getitem__(0).getNombre()
-            cadena = f", la zona en la que me ubico es {nombre}, en el {self._zona[0].getZoo().getNombre()}"
-            return cadena
-        return f"Mi nombre es {self._nombre}, tengo una edad de {self._edad}, habito en {self._habitat} y mi genero es {self._genero}"
+    def cantidadReptiles (cls):
+        return cls.total_reptiles
     
-    def setNombre(self, nombre):
-        self._nombre = nombre
+    @classmethod
+    def crearIguana(cls, nombre = "", edad = 0, habitat = "", genero = "" ):
+        cls.iguanas += 1
+        iguana = Reptil(nombre, edad, "humedal", genero, "verde", 3)
 
-    def getNombre(self):
-        return self._nombre 
-
-    def setEdad (self, edad):
-        self._edad = edad
-
-    def getEdad (self):
-        return self._edad
+    @classmethod
+    def crearSerpiente (cls, nombre = "", edad = 0, habitat = "", genero = "" ):
+        cls.serpientes += 1
+        serpiente = Reptil(nombre, edad, "jungla", genero, "blanco", 1)
     
-    def setHabitat (self, habitat):
-        self._habitat = habitat
+    @classmethod
+    def getListado (cls):
+        return cls._listado
 
-    def getHabitat (self):
-        return self._habitat
+    def setColorEscamas (self, color):
+        self._colorEscamas = color
+
+    def getColorEscamas (self):
+        return self._colorEscamas
     
-    def getGenero (self):
-        return self._genero
     
-    def setGenero (self, genero):
-        self._genero = genero
+    def setLargoCola(self, largo):
+        self._largoCola =  largo 
+    
+    def getLargoCola(self):
+        return self._largoCola
